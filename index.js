7d0b92c54b5cfb50923898668eb52a0e8ba97ed0
damian <damiankohut@gmail.com>
2021-11-10 15:55:04 +0000

    finish question

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //write your code here
+const stripUpperCase = (string) => {
+let newString = ""
+for (let i = 0; i <= string.length; i++ ){
+    console.log(string[i])
+} 
+};
+//check if the string have uppercase letters
+//if the string have uppercase letters remove those letters
+//print out the new array without the cuppercase letters
+
+console.log(stripUpperCase("Hello"))
+
+// let newsting = "" put all lowercase letters in that string
+//for loop to iterate through the string
+//remove uppercase characters
+//add the character to the end of newString
+//return newstring
